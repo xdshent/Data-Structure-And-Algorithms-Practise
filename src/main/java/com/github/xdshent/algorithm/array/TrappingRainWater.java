e1849538b145ef2b89c0304ae36599816a5f2de4
xdshen <xdshent@gmail.com>
2018-06-27 22:07:06 +0800

    trapping rain water desc

---
+++ src/main/java/com/github/xdshent/algorithm/array/TrappingRainWater.java
@@ -0,0 +1,24 @@
+package com.github.xdshent.algorithm.array;
+
+/**
+ * Trapping Rain Water within given set of bars
+ * In trapping rain water problem, we need to find the
+ * maximum amount of water that can be trapped within
+ * given set of bars where width of each bar is 1 unit.
+ */
+public class TrappingRainWater {
+    /**
+     * The idea is to calculate maximum height bar on
+     * the left and right of every bar. Then the amount
+     * of water that can be stored on top of each bar
+     * is equal to minimum among maximum bar to the
+     * left and right minus height of current bar.
+     *
+     * @param bars
+     * @return
+     */
+    public static int trap1(int[] bars) {
+
+        return 0;
+    }
+}
