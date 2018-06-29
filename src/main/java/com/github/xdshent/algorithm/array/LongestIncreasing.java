5c05e4aba604b752bf68473e011886c0a8bddbd6
xdshen <xdshent@gmail.com>
2018-06-29 22:23:35 +0800

    longest increasing desc

---
+++ src/main/java/com/github/xdshent/algorithm/array/LongestIncreasing.java
@@ -0,0 +1,13 @@
+package com.github.xdshent.algorithm.array;
+
+/**
+ * Longest Increasing Sub Sequence
+ * The longest increasing sub sequence problem is to
+ * find a sub sequence of a given sequence in which
+ * the sub sequence's elements are stored order,
+ * lowest to highest, and in which the sub sequence is
+ * as long as problem. This sub sequence is not
+ * necessarily contiguous, or unique.
+ */
+public class LongestIncreasing {
+}
