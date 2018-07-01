830ec5a6c803b66bd4ec70c127d1c230787952d9
xdshen <xdshent@gmail.com>
2018-07-01 23:06:20 +0800

    longest increasing desc-1

--- src/main/java/com/github/xdshent/algorithm/array/LongestIncreasing.java
+++ src/main/java/com/github/xdshent/algorithm/array/LongestIncreasing.java
@@ -8,6 +8,19 @@ package com.github.xdshent.algorithm.array;
  * lowest to highest, and in which the sub sequence is
  * as long as problem. This sub sequence is not
  * necessarily contiguous, or unique.
+ * <p>
+ * For example,
+ * consider sub sequence {0,8,4,12,2,10,6,14,1,9,5,13,3,11,7,15}
+ * The longest increasing sub sequence is{0,2,6,9,11,15}
+ * <p>
+ * This sub sequence has length 6; the input sequence has no 7-member
+ * increasing sub sequence. The longest increasing sub sequence in this
+ * example is not unique: for instance,
+ * {0,4,6,9,11,15} or
+ * {0,4,6,9,13,15}
+ * are other increasing sub sequence of equal length in the same input
+ * sequence.
+ *
  */
 public class LongestIncreasing {
 }
