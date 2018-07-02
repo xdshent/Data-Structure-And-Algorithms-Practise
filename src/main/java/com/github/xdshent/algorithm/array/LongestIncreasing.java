35be7437e4ef01589f47245e95cff806d5308171
xdshen <xdshent@gmail.com>
2018-07-02 22:18:15 +0800

    longest increasing desc-2

--- src/main/java/com/github/xdshent/algorithm/array/LongestIncreasing.java
+++ src/main/java/com/github/xdshent/algorithm/array/LongestIncreasing.java
@@ -10,17 +10,65 @@ package com.github.xdshent.algorithm.array;
  * necessarily contiguous, or unique.
  * <p>
  * For example,
- * consider sub sequence {0,8,4,12,2,10,6,14,1,9,5,13,3,11,7,15}
+ * consider sub sequence {0,8,4,12,2,10,6,14,1,9,5,13,
+ * 3,11,7,15}
  * The longest increasing sub sequence is{0,2,6,9,11,15}
  * <p>
- * This sub sequence has length 6; the input sequence has no 7-member
- * increasing sub sequence. The longest increasing sub sequence in this
+ * This sub sequence has length 6; the input sequence
+ * has no 7-member
+ * increasing sub sequence. The longest increasing sub
+ * sequence in this
  * example is not unique: for instance,
  * {0,4,6,9,11,15} or
  * {0,4,6,9,13,15}
- * are other increasing sub sequence of equal length in the same input
+ * are other increasing sub sequence of equal length
+ * in the same input
  * sequence.
- *
+ * <p>
+ * We have already discussed a O(N^2) time complexity
+ * solution of LIS here which uses Dynamic Programming
+ * .In this post, a O(NlogN) time
+ * Non-DP solution is discussed.
+ * <p>
+ * Let S[i] be defined as the smallest integer that
+ * ends an increasing sequence of length i. Now
+ * iterate through every integer X of the
+ * input set and do the following:
+ * <p>
+ * If X is more than the last element in S, then
+ * append X to the end of S. This essentially means we
+ * have found a new largest LIS.
+ * <p>
+ * Otherwise find the smallest element in S, which is
+ * more than or equal to X, and replace it with X.
+ * Because S is sorted at any time, the element can be
+ * found using binary search in log(N) time.
+ * <p>
+ * Let's illustrate this with the help of a example.
+ * Consider below
+ * array of integers
+ * {2,6,3,4,1,2,9,5,8}
+ * Below are the steps followed by the algorithm:
+ * Initialize to an empty set S = {}
+ * <p>
+ * Inserting 2: S = {2} New largest LIS
+ * Inserting 6: S = {2,6} New largest LIS
+ * Inserting 3: S = {2,3} Replace 6 with 3
+ * Inserting 4: S = {2,3,4} New largest LIS
+ * Inserting 1: S = {1,3,4} Replace 2 with 1
+ * Inserting 2: S = {1,2,4} Replace 3 with 2
+ * Inserting 9: S = {1,2,4,9} New largest LIS
+ * Inserting 5: S = {1,2,4,5} Replace 9 with 5
+ * Inserting 8: S = {1,2,4,5,8} New largest LIS
+ * <p>
+ * So, the length of the LIS is 5(the size of S).
+ * Please note than here
+ * S[i] is defined as the smallest integer that ends
+ * an increasing
+ * sequence of length i. Therefore, S does not
+ * represent an actual
+ * sequence but the size of S represents the length of
+ * the LIS.
  */
 public class LongestIncreasing {
 }
