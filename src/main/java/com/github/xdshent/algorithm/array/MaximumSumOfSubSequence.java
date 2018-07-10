635e52bd44d9a66863a0a02f2a3e566812e72a73
xdshen <xdshent@gmail.com>
2018-07-10 20:56:58 +0800

    maximum sum of sub sequence with no adjacent element part-2

--- src/main/java/com/github/xdshent/algorithm/array/MaximumSumOfSubSequence.java
+++ src/main/java/com/github/xdshent/algorithm/array/MaximumSumOfSubSequence.java
@@ -30,7 +30,7 @@ public class MaximumSumOfSubSequence {
      * @param prev
      * @return
      */
-    public static int maxSumSubSeq(int[] arr, int i, int n, int prev) {
+    public static int maxSumSubSeq1(int[] arr, int i, int n, int prev) {
 
         //base case: all elements are processed
         if (i == n) {
@@ -38,16 +38,63 @@ public class MaximumSumOfSubSequence {
         }
 
         //recurse by excluding current element
-        int excl = maxSumSubSeq(arr, i + 1, n, prev);
+        int excl = maxSumSubSeq1(arr, i + 1, n, prev);
 
         int incl = 0;
 
         //include current element only if it is not adjacent to previous element considered
         if (prev + 1 != i) {
-            incl = maxSumSubSeq(arr, i + 1, n, i) + arr[i];
+            incl = maxSumSubSeq1(arr, i + 1, n, i) + arr[i];
         }
 
         //return maximum sum we get by including or excluding current item
         return Integer.max(incl, excl);
+    }
+
+    /**
+     * We can also solve this problem in bottom-up fashion using Dynamic
+     * Programming(Tabulation). In the bottom-up approach, we solve
+     * smaller sub-problems first, then solve larger sub-problems from
+     * them. The idea is to create an auxiliary array lookup[] to store
+     * solution of sub-problems where for each index i, lookup[i] stores
+     * the maximum value that can be attained till index i. It uses
+     * value of smaller values i already computed. It has the same
+     * asymptotic run-time as Memoization but no recursion overhead.
+     * <p>
+     * time complexity: O(N)
+     * auxiliary space used: O(N)
+     *
+     * @param array
+     * @return
+     */
+    public static int maxSumSubSeq2(int[] array) {
+
+        int n = array.length;
+
+        //base case
+        if (n == array.length) {
+            return array[0];
+        }
+
+        //create an auxiliary array to store solution of sub-problems
+        int[] lookup = new int[n];
+
+        //lookup[i] stores the maximum sum possible till index i
+
+        //trivial case
+        lookup[0] = array[0];
+        lookup[1] = Integer.max(array[0], array[1]);
+
+        for (int i = 2; i < n; i++) {
+            //lookup[i] stores the maximum sum we get by
+            //1: excluding current element & take maximum sum till index i-1
+            //2: including current element array[i] and take maximum sum till index-2
+            lookup[i] = Integer.max(lookup[i - 1], lookup[i - 2] + array[i]);
+
+            lookup[i] = Integer.max(lookup[i], array[i]);
+        }
+
+        //return maximum sum
+        return lookup[n - 1];
     }
 }
