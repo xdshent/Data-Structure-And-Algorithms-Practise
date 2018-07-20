318d98df8ac4908310fb458a1d32836d97a9c135
xdshen <xdshent@gmail.com>
2018-07-20 23:54:46 +0800

    feat:swap two mistake elements
    an array in one swap whose two elements are swapped by mistake

---
+++ src/main/java/com/github/xdshent/algorithm/array/SortAnArray.java
@@ -0,0 +1,10 @@
+package com.github.xdshent.algorithm.array;
+
+/**
+ * Sort an array in one swap whose two elements are swapped by mistake
+ * Given an array where all its elements are sorted except two elements
+ * which were swapped, sort the array in linear time. Assume there are
+ * no duplicates in the array.
+ */
+public class SortAnArray {
+}
