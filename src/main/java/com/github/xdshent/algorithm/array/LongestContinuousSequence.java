373b47f621b866ead05de2b428bb3e22cd2a89fd
xdshen <xdshent@gmail.com>
2018-07-25 23:35:51 +0800

    feat: longest continuous sequence with same sum desc
    length of longest continuous sequence with same sum in given binary arrays

---
+++ src/main/java/com/github/xdshent/algorithm/array/LongestContinuousSequence.java
@@ -0,0 +1,12 @@
+package com.github.xdshent.algorithm.array;
+
+/**
+ * Length of longest continuous sequence with same sum in given binary arrays
+ * <p>
+ * Given two Boolean arrays X and Y, find the length of longest continuous sequence that starts and
+ * ends at same index in both arrays and have same sum. In other words, find max(j-i+1) for every
+ * j>=i where sum of sub-array X[i,j] is equal to sum of sub-array Y[i,j].
+ */
+public class LongestContinuousSequence {
+
+}
