dd70868c1426c777dfd21339aa900b1e2e0ab39a
xdshen <xdshent@gmail.com>
2018-08-28 20:42:34 +0800

    feat: duplication in array
    duplication in array

--- src/main/java/com/github/xdshent/algorithm/array/DuplicationInArray.java
+++ src/main/java/com/github/xdshent/algorithm/array/DuplicationInArray.java
@@ -30,4 +30,57 @@ public class DuplicationInArray {
         }
         return false;
     }
+
+    /**
+     * @param array
+     * @return
+     */
+    public static int duplication2(int[] array) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+
+        int start = 0;
+        int end = array.length - 1;
+        while (end >= start) {
+            int middle = ((end - start) >> 1) + start;
+
+            int count = countRange(array, start, middle);
+
+            if (end == start) {
+                if (count > 1) {
+                    return start;
+                } else {
+                    break;
+                }
+            }
+
+            if (count > (middle - start + 1)) {
+                end = middle;
+            } else {
+                start = middle + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * @param array
+     * @param start
+     * @param middle
+     * @return
+     */
+    private static int countRange(int[] array, int start, int middle) {
+        if (array == null) {
+            return 0;
+        }
+
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] >= start && array[i] <= middle) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
